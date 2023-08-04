3e10a8fcf58c956634ee3ffe3ff242b1a7f62d2c
Moyasi Ginko <mrsiyam61@gmail.com>
2023-08-05 04:04:20 +0600

    Create class_trimmer_decorator.rb

---
+++ class_trimmer_decorator.rb
@@ -0,0 +1,8 @@
+# Purpose: TrimmerDecorator class
+
+class TrimmerDecorator < Decorator
+  def correct_name
+    name = @nameable.correct_name
+    name.length > 10 ? name[0...10] : name
+  end
+end
