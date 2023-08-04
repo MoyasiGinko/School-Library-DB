a068db631a95789de67235b6fcd0d96ca444fc05
Moyasi Ginko <mrsiyam61@gmail.com>
2023-08-05 05:30:05 +0600

    Delete class_trimmer_decorator.rb

--- class_trimmer_decorator.rb
+++
@@ -1,8 +0,0 @@
-# Purpose: TrimmerDecorator class
-
-class TrimmerDecorator < Decorator
-  def correct_name
-    name = @nameable.correct_name
-    name.length > 10 ? name[0...10] : name
-  end
-end
