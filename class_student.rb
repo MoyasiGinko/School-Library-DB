d33237786206401f9237f3c90945164a1cc0a195
Moyasi Ginko <mrsiyam61@gmail.com>
2023-08-05 05:30:24 +0600

    Update class_student.rb

--- class_student.rb
+++ class_student.rb
@@ -3,11 +3,16 @@
 require './class_person' # Import the Person class from the class_person file
 
 class Student < Person
-  attr_accessor :classroom # Add this line to declare the classroom variable as an accessor
+  attr_accessor :name, :classroom # Add this line to declare the classroom variable as an accessor
 
   def initialize(age, classroom, name = 'Unknown', parent_permission: false)
     super(age, name, parent_permission: parent_permission)
     @classroom = classroom
+  end
+
+  def classroom=(classroom)
+    @classroom = classroom
+    classroom.students << self unless classroom.students.include?(self)
   end
 
   def play_hooky
