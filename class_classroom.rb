159b0dbec8521d70cc4dacf7d64569c2f186920c
Moyasi Ginko <mrsiyam61@gmail.com>
2023-08-05 06:37:21 +0600

    Update class_classroom.rb

--- class_classroom.rb
+++ class_classroom.rb
@@ -7,8 +7,9 @@ class Classroom
   end
 
   # Method to add a student to the classroom
-  def add_student(student)
-    students << student
-    student.classroom = self
+  def add_student(age, name)
+    student = Student.new(age, self, name)
+    @students << student
+    student
   end
 end
