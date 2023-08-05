3edf7e4010c3f874d6267600c90987bc2b4265ad
Moyasi Ginko <mrsiyam61@gmail.com>
2023-08-05 06:37:40 +0600

    Update class_rental.rb

--- class_rental.rb
+++ class_rental.rb
@@ -1,7 +1,14 @@
 class Rental
-  attr_accessor :date
+  attr_accessor :date, :book, :person
 
-  def initialize(date)
+  def initialize(date, book, person)
     @date = date
+    @book = book
+    @person = person
+
+    # Add the rental to the book's rentals
+    book.add_rental(self)
+    # Add the rental to the person's rentals
+    person.add_rental(self)
   end
 end
