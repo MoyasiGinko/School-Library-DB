e030509ac3703f5e35138f3320772d96f153c5d2
Moyasi Ginko <mrsiyam61@gmail.com>
2023-08-06 03:55:49 +0600

    Refactored and updated the code

--- app.rb
+++ app.rb
@@ -18,7 +18,7 @@ class App
     puts '=== List of Books ==='
     puts 'Title, Author'
     puts '--------------------------'
-    @books.each { |book| puts "#{book.title}, #{book.author}" }
+    @books.each_with_index { |book, index| puts "#{index}, #{book.title}, #{book.author}" }
   end
 
   def create_classroom(label)
@@ -65,7 +65,12 @@ class App
 
     rental = person.add_rental(book, date)
     @rentals << rental
-    puts "Rental created: #{person.name} rented #{book.title} on #{rental.date}."
+    puts "\nRental created: #{person.name} rented #{book.title} on #{rental.date}."
+  end
+
+  def add_rental_date
+    print 'Enter the rental date (YYYY-MM-DD): '
+    gets.chomp
   end
 
   def list_rentals_for_person(person_id)
@@ -80,7 +85,74 @@ class App
     end
   end
 
-  private
+  # Display menu options to the user
+  def menu
+    puts "\nChoose an option:"
+    puts '1. List all books'
+    puts '2. List all people'
+    puts '3. Create a person'
+    puts '4. Create a book'
+    puts '5. Create a rental'
+    puts '6. List rentals for a person (by ID)'
+    puts '7. Quit'
+    print 'Enter the option number: '
+  end
+
+  def create_person_option
+    print 'Enter age: '
+    age = gets.chomp.to_i
+    print 'Enter name: '
+    name = gets.chomp
+    print 'Enter type (student/teacher): '
+    type = gets.chomp.downcase
+    if type == 'student'
+      print 'Enter classroom label: '
+      classroom_label = gets.chomp
+    else
+      classroom_label = nil
+    end
+    print 'Does the person have parent permission? (true/false): '
+    parent_permission = gets.chomp.downcase == 'true'
+    create_person(age, name, type, classroom_label, parent_permission: parent_permission)
+  end
+
+  def create_book_option
+    print 'Enter book title: '
+    title = gets.chomp
+    print 'Enter book author: '
+    author = gets.chomp
+    create_book(title, author)
+  end
+
+  def create_rental_option
+    puts 'Select the book by index:'
+    @books.each_with_index { |book, index| puts "#{index + 1}. #{book.title}, #{book.author}" }
+    print 'Enter the book index: '
+    book_index = gets.chomp.to_i
+
+    puts 'Select the person by index:'
+    @people.each_with_index { |person, index| puts "#{index + 1}. #{person.name}" }
+    print 'Enter the person index: '
+    person_index = gets.chomp.to_i
+
+    book = @books[book_index - 1]
+    person = @people[person_index - 1]
+
+    if book && person
+      rental_date = add_rental_date
+      create_rental(rental_date, book.title, person.name)
+    else
+      puts 'Book or person not found. Please try again.'
+    end
+  end
+
+  def list_rentals_for_person_option
+    puts "\n---Choose a person by their ID---\n\n"
+    list_all_people
+    print 'Enter person ID: '
+    person_id = gets.chomp.to_i
+    list_rentals_for_person(person_id)
+  end
 
   def find_classroom_by_label(label)
     @classrooms.find { |classroom| classroom.label == label }
--- main.rb
+++ main.rb
@@ -1,9 +1,7 @@
-# main.rb
+# main.rb (Refactored)
 require_relative 'app'
 
-def main
-  app = App.new
-
+def create_sample_data(app)
   # Create some classrooms
   app.create_classroom('NeuroScience')
   app.create_classroom('Math 101')
@@ -17,71 +15,39 @@ def main
   # Create some people
   app.create_person(23, 'John Doe', 'student', 'NeuroScience')
   app.create_person(18, 'Jane Smith', 'student', 'Math 101')
-  app.create_person(43, 'Henry', 'teacher', nil, parent_permission: true) # Don't specify classroom for teacher
+  app.create_person(43, 'Henry', 'teacher', nil, parent_permission: true)
+end
 
-  # Create some rentals
-  app.create_rental('2018-2-13', 'Loving Intro', 'John Doe')
-  app.create_rental('2018-2-13', 'Loving Intro', 'Jane Smith')
-  app.create_rental('2020-3-1', "Let's build the world", 'John Doe')
-  app.create_rental('2022-1-19', "Let's go to mars", 'Jane Smith')
-  app.create_rental('2022-8-19', 'The end of hating story', 'Jane Smith')
+def handle_option(app, option)
+  option_actions = {
+    1 => -> { app.list_all_books },
+    2 => -> { app.list_all_people },
+    3 => -> { app.create_person_option },
+    4 => -> { app.create_book_option },
+    5 => -> { app.create_rental_option },
+    6 => -> { app.list_rentals_for_person_option }
+  }
+
+  if option_actions.key?(option)
+    option_actions[option].call
+  elsif option == 7
+    return true # Exit the loop
+  else
+    puts 'Invalid option. Try again.'
+  end
+
+  false # Continue the loop
+end
+
+def main
+  app = App.new
+  create_sample_data(app)
 
   loop do
-    puts "\nChoose an option:"
-    puts '1. List all books'
-    puts '2. List all people'
-    puts '3. Create a person'
-    puts '4. Create a book'
-    puts '5. Create a rental'
-    puts '6. List rentals for a person (by ID)'
-    puts '7. Quit'
-    print 'Enter the option number: '
+    app.menu
     option = gets.chomp.to_i
 
-    case option
-    when 1
-      app.list_all_books
-    when 2
-      app.list_all_people
-    when 3
-      print 'Enter age: '
-      age = gets.chomp.to_i
-      print 'Enter name: '
-      name = gets.chomp
-      print 'Enter type (student/teacher): '
-      type = gets.chomp.downcase
-      if type == 'student'
-        print 'Enter classroom label: '
-        classroom_label = gets.chomp
-      else
-        classroom_label = nil
-      end
-      print 'Does the person have parent permission? (true/false): '
-      parent_permission = gets.chomp.downcase == 'true'
-      app.create_person(age, name, type, classroom_label, parent_permission: parent_permission)
-    when 4
-      print 'Enter book title: '
-      title = gets.chomp
-      print 'Enter book author: '
-      author = gets.chomp
-      app.create_book(title, author)
-    when 5
-      print 'Enter rental date: '
-      date = gets.chomp
-      print 'Enter book title: '
-      book_title = gets.chomp
-      print 'Enter person name: '
-      person_name = gets.chomp
-      app.create_rental(date, book_title, person_name)
-    when 6
-      print 'Enter person ID: '
-      person_id = gets.chomp.to_i
-      app.list_rentals_for_person(person_id)
-    when 7
-      break
-    else
-      puts 'Invalid option.'
-    end
+    break if handle_option(app, option)
   end
 end
 
